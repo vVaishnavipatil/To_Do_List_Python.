7248f78958ad145610ce6a32a5e8b6ee590f9ee6
Vaishnavi Motilal Patil <137201737+vVaishnavipatil@users.noreply.github.com>
2023-09-29 20:47:47 +0530

    Add files via upload
    GUI based To Do List Application Python.
    IDE - PyCharm
    Language - Python

---
+++ To do list.py
@@ -0,0 +1,9 @@
+Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import tkinter
+... from tkinter import*
+... 
+... root=Tk()
+... root.title("To-Do-List")
+... root.gometry("400x650+400+100")
+... root.resizable(False,False)
